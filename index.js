c4d9fc4b6c21b303f26cbff5160fbe98209ec07a
Tom Hinton <tomjhinton@gmail.com>
2020-01-30 21:47:21 +0000

    added script

--- index.js
+++ index.js
@@ -3,18 +3,7 @@ const canvas = createCanvas(800, 800)
 const ctx = canvas.getContext('2d')
 require('dotenv').config()
 var Twitter = require('twitter');
-// Write "Awesome!"
-// ctx.font = '30px Impact'
-// ctx.rotate(0.1)
-// ctx.fillText('Awesome!', 50, 100)
-//
-// // Draw line under text
-// var text = ctx.measureText('Awesome!')
-// ctx.strokeStyle = 'rgba(0,0,0,0.5)'
-// ctx.beginPath()
-// ctx.lineTo(50, 102)
-// ctx.lineTo(50 + text.width, 102)
-// ctx.stroke()
+const PORT = process.env.PORT || 3000;
 
 
 function draw2(startX, startY, len, angle) {
